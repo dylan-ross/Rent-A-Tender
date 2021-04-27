736ba2acf69f9f7a436309384804791eb6c11722
Dylan Ross <dylanross@Dylans-MacBook-Pro.local>
2021-04-27 14:14:04 -0400

    starting authorization

--- app/models/bartender.rb
+++ app/models/bartender.rb
@@ -1,3 +1,15 @@
 class Bartender < ApplicationRecord
+  has_secure_password
   has_and_belongs_to_many :jobs
+
+  validates :name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :category, presence: true
+  validates :intro, presence: true
+  validates :bio, presence: true 
+  validates :skills, presence: true
+  validates :rate, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, length: { minimum: 6 }
 end
