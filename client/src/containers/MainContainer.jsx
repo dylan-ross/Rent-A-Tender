a01cff19b0fc887cf275f4f5f258327258770c06
Dylan Ross <dylanross@Dylans-MacBook-Pro.local>
2021-04-27 18:15:45 -0400

    set up apiConfig

---
+++ client/src/containers/MainContainer.jsx
@@ -0,0 +1,8 @@
+import { useState, useEffect } from 'react';
+import { Switch, Route, useHistory } from 'react-router-dom';
+import Bartenders from '../screens/bartenders/Bartenders';
+import BartenderDetail from '../screens/bartenderDetail/BartenderDetail';
+import BartenderProfileForm from '../screens/bartenderProfileForm/bartenderProfileForm';
+import BartenderJobs from '../screens/bartenderJobs/BartenderJobs';
+import Confirmation from '../screens/confirmation/Confirmation';
+import UserEvents from '../screens/userEvents/UserEvents'
