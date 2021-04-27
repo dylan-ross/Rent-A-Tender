2be51726e9fabafdd891d7a8f2f9d9b393ee3d17
Dylan Ross <dylanross@Dylans-MacBook-Pro.local>
2021-04-27 15:23:45 -0400

    CORS

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,16 +4,16 @@ class UsersController < ApplicationController
 
 
   # GET /users
-  def index
-    @users = User.all
+  # def index
+  #   @users = User.all
 
-    render json: @users
-  end
+  #   render json: @users
+  # end
 
   # GET /users/1
-  def show
-    render json: @user
-  end
+  # def show
+  #   render json: @user
+  # end
 
   # POST /users
   def create
@@ -31,18 +31,18 @@ class UsersController < ApplicationController
   end
 
   # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
+  # def update
+  #   if @user.update(user_params)
+  #     render json: @user
+  #   else
+  #     render json: @user.errors, status: :unprocessable_entity
+  #   end
+  # end
 
   # DELETE /users/1
-  def destroy
-    @user.destroy
-  end
+  # def destroy
+  #   @user.destroy
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
