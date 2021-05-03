15172d4b93ed6ae537877b0c487543c1efd81bc3
Dylan Ross <dylanross@Dylans-MacBook-Pro.local>
2021-05-03 14:19:28 -0400

    some conditional rendering

--- client/src/screens/home/Home.jsx
+++ client/src/screens/home/Home.jsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom"
 import logo from "../../components/assets/images/small-logo.png";
-
+import "../home/Home.css"
 export default function Home () {
 
   return (
     <div className="home">
-      <Link to="/bartenders">
+      <Link className="logo-link" to="/bartenders">
       <img className="small-logo" src={logo} alt="link to bartenders" />
       </Link>
     </div>
